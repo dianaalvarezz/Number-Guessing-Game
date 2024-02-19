0a85250839723bf1edffa996e0744fbfd2047ea8
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-02-19 12:27:38 -0600

    Update python.py
    started coding the game

--- python.py
+++ python.py
@@ -1,1 +1,24 @@
+import random
+import math
+
+lowernumber = int(input("Enter the lower bound number: "))
+uppernumber = int(input("Enter the upper bound number "))
+
+x = random.randint(lowernumber, uppernumber)
+print("\n\tYou have only ",
+    round(math.log(uppernumber - lowernumber + 1, 2)),
+    " chances to guess the integer!\n")
+ 
+guesses = 0
+
+while guesses < math.log(uppernumber - lowernumber + 1, 2):
+    guesses += 1
+
+    guess = int(input("Guess a number:- "))
+
+    if x == guess:
+        print("Congratulations you did it in ",
+              guesses, " tries")
+        
+        m
 
