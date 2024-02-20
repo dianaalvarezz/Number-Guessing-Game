381b028c0e5dc6ed618771f29e8295d1e2e12441
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-02-20 11:50:08 -0600

    Update main.py
    finished documentation

--- main.py
+++ main.py
@@ -12,28 +12,29 @@ import math #provides logarithm function needed to calculate number of guesses b
 lowernumber = int(input("Enter the lower bound number: "))
 uppernumber = int(input("Enter the upper bound number "))
 
+# Generates the number that is to be guesssed within the specified range
 numtoguess = random.randint(lowernumber, uppernumber)
-print("\n\tYou have only ",
-    round(math.log(uppernumber - lowernumber + 1, 2)),
-    " chances to guess the integer!\n")
+
+# Informs the player of the amount of chances they have
+print("\n\tYou have only ", round(math.log(uppernumber - lowernumber + 1, 2)), " chances to guess the integer!\n")
  
-guesses = 0
+guesses = 0 #Initialize the number of guesses that will be taken
 
+# Loop that will continue until the user runs out of guesses
 while guesses < math.log(uppernumber - lowernumber + 1, 2):
-    guesses += 1
+    guesses += 1 # Increments the amount of guesses
+    guess = int(input("Guess a number:- ")) # Promts player the guess 
 
-    guess = int(input("Guess a number:- "))
-
+    # Checks to see if the guess was correct, too high, or too low
     if numtoguess == guess:
-        print("Congratulations you did it in ",
-              guesses, " tries")
-        
+        print("Congratulations you did it in ", guesses, " tries")
         break
     elif numtoguess > guess:
         print("The number you guesses is too small!")
     elif numtoguess < guess:
         print("The number you guesses is too large!")
 
+# If player runs out of chances than allowed the game will give themm the correct answer
 if guesses > math.log(uppernumber - lowernumber + 1, 2):
         print("\nThe number was ", numtoguess, ". Nice try!")
         
